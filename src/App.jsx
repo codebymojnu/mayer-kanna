f1049887f243bb1ebfb7d802f2af2174346796eb
mojnuapp <thisismojnu@gmail.com>
2024-01-17 00:24:44 +0600

    prevent default use for stop reload page when submit

--- src/App.jsx
+++ src/App.jsx
@@ -1,47 +1,5 @@
-import { useState } from "react";
+import Form from "./Form";
 
-export function FamilyMember({ member: { name, relation }, children }) {
-  const [somporko, setSomporko] = useState("");
-  const [family, setFamily] = useState("");
-
-  function handleFamilyClick() {
-    const text = "Family";
-    setFamily(text);
-  }
-
-  function handleClick(relation) {
-    setSomporko(relation);
-  }
-  return (
-    <div
-      style={{ border: "1px solid red", padding: "10px", margin: "10px" }}
-      onClick={() => handleFamilyClick()}
-    >
-      {family}
-      <p
-        style={{ border: "1px solid black", padding: "1px" }}
-        onClick={(e) => {
-          handleClick(relation);
-          e.stopPropagation();
-        }}
-      >
-        {children} <span>{somporko}</span>
-      </p>
-    </div>
-  );
+export default function App() {
+  return <Form />;
 }
-
-function App() {
-  return (
-    <>
-      <FamilyMember member={{ name: "Shahadot", relation: "father" }}>
-        Shahadot
-      </FamilyMember>
-      <FamilyMember member={{ name: "Monju", relation: "mother" }}>
-        Monju
-      </FamilyMember>
-    </>
-  );
-}
-
-export default App;
